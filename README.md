9d9e6f544042188bec6fef9a4decd243e016285c
Filipponi, Luca <luca.filipponi89@gmail.com>
2018-06-26 16:44:41 +0100

    changed name and added a small description in the readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# AsyncRestService
+# NonBlockingRestService
 
-Sample study implementation for a rest services using non blocking libraries
+Sample study implementation for a rest services using reactive library. The idea is to create a rest service that is non-blocking, in this way we will have a better
+resource utilisation.
+
+In this example I wille rxScala, that is scala wrapper around RxJava developed by netflix.
+
+```https://github.com/ReactiveX/RxScala```
+
+The spring boot will be use for packaging/running the application.
--- src/main/scala/com.filipponi.SpringBootScalaFull/resource/Resource.scala
+++ src/main/scala/com.filipponi.SpringBootScalaFull/resource/Resource.scala
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, R
 @RestController
 class Resource {
 
+
+  /**
+    *
+    * I need support from the framework for realeasing the tomcat thread while waiting otherwise there is no differnce using
+    * the reactive framework or not.
+    *
+    * @return
+    */
   @RequestMapping(path = Array("/hello"),
     method = Array(RequestMethod.GET),
     produces = Array("application/json"))
-  def getNewBoard(): ResponseEntity[String] = {
+  def hello(): ResponseEntity[String] = {
     ResponseEntity.ok("Hello!")
   }
 
